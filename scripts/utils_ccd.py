84b91db527e32ca8b108a2267646124a3e1dd5a7
Eduardo P Lima <eduardolima@tce.rn.gov.br>
2025-06-11 15:30:47 -0300

    para wsl

--- scripts/utils_ccd.py
+++ scripts/utils_ccd.py
@@ -1,7 +1,11 @@
 import pymssql
 import os
+import pypdf
 
 from dotenv import load_dotenv
+from pathlib import Path
+
+DIR_INFORMACOES = '/media/informacoes_pdf/'
 
 def get_connection(db='processo'):
     load_dotenv()
@@ -12,3 +16,18 @@ def get_connection(db='processo'):
         port=os.getenv("SQL_SERVER_PORT"),
         database=db
     )
+
+def extract_text_from_pdf(file_path):
+    try:
+        with open(file_path, 'rb') as f:
+            reader = pypdf.PdfReader(f)
+            text = ''
+            for page in reader.pages:
+                text += page.extract_text() or ''
+            return text
+    except Exception as e:
+        print(f"Error reading {file_path}: {e}")
+        return ''
+
+def get_file_path(row):
+    return Path(DIR_INFORMACOES) / row['setor'].strip() / row['arquivo']
